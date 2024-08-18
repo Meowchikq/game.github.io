031b292a5240b859e3a630308b65060ff17a3360
Meowchikq <165686290+Meowchikq@users.noreply.github.com>
2024-08-18 15:20:44 +0300

    Update clicker.js

--- clicker.js
+++ clicker.js
@@ -1,12 +1,14 @@
 let clicks=0
 var block=document.querySelector('img');
+function updt() {
+	document.getElementById('clicks').textContent = 'Блоков: ' + clicks;
+};
 block.onclick=function() {
 	var mysrc=block.getAttribute('src');
 	if (mysrc==='block.png') {
 		block.setAttribute('src', 'block2.png');
-		setTimeout(()=>{block.setAttribute('src', 'block.png');},100);
+		setTimeout(()=>{block.setAttribute('src', 'block.png');},50);
+		clicks+=1;
+		updt();
 	}
 };
-
-	
-		
